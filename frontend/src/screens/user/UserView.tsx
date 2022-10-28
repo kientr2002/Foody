23c8430a5d36e6f0b4c6f09e25084377d2fefff1
thoaile <lenguyenhuyenthoai@gmail.com>
2022-10-28 10:52:07 +0700

    recode navbar using bottom nagivation tabs & change tab header style & testing screen view

---
+++ frontend/src/screens/user/UserView.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import Navbar from '../../components/navbar/Navbar'
+import Home from './Home/Home'
+import Plan from './Plan/Plan'
+import Search from './Search/Search'
+import Favorite from './Favorite/Favorite'
+import Profile from './Profile/Profile'
+import color from '../../styles/color'
+
+const Tab = createBottomTabNavigator()
+
+export default function UserView() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator 
+                tabBar={props => <Navbar {...props}/>}
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: color.background
+                    },
+                    headerTintColor: color.primary,
+                    headerTitleStyle: {
+                        fontFamily: 'SF-Pro-Rounded_heavy',
+                        fontSize: 25
+                    }
+                }}
+            >
+                <Tab.Screen name='Home' component={Home} />
+                <Tab.Screen name='Plan' component={Plan} />
+                <Tab.Screen name='Search' component={Search} />
+                <Tab.Screen name='Favorite' component={Favorite} />
+                <Tab.Screen name='Profile' component={Profile} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
