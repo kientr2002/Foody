8ea7443ed72845ce8f4baea70a3fe7573e837d31
thoaile <lenguyenhuyenthoai@gmail.com>
2022-11-02 21:19:13 +0700

    coding home screen

---
+++ frontend/src/screens/user/UserView.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { UserTabParamList } from '../../util/types'
+
+import Navbar from '../../components/navbar/Navbar'
+
+import color from '../../styles/color'
+
+const Tab = createBottomTabNavigator<UserTabParamList>()
+const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: color.background
+    }
+}
+
+export default function UserView() {
+    return (
+        <NavigationContainer
+            theme={MyTheme}
+        >
+            <Tab.Navigator 
+                tabBar={props => <Navbar {...props}/>}
+                screenOptions={{
+                    headerShown: false
+                }}
+            >
+                <Tab.Screen name='Home' component={Home} />
+                <Tab.Screen name='Plan' component={Plan} />
+                <Tab.Screen name='Search' component={Search} />
+                <Tab.Screen name='Favorite' component={Favorite}/>
+                <Tab.Screen name='Profile' component={Profile}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
