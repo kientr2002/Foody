c18126062b9d048caf466fdbf0790762b78385d8
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2022-11-28 18:24:14 +0700

    update Caculate, Dropdown

--- frontend/src/screens/authentication/calculate/Calculate.tsx
+++ frontend/src/screens/authentication/calculate/Calculate.tsx
@@ -7,35 +7,88 @@ import Input from '../../../components/input/Input'
 import styles from './styles'
 import Dropdown from '../../../components/dropdown/Dropdown'
 
+
+
+var outputHeight = 0
+var outputWeight = 0
+var outputActivity = 0
+var outputTarget = 0
+
 export default function Calculate({ navigation }: any) {
     const targets = ['Increase Weight', 'Reduce Weight', 'Keep This Weight']
     const [success, setSuccess] = React.useState<boolean>(false)
     const [warningWeight, setWarningWeight] = React.useState<string>('')
     const [warningHeight, setWarningHeight] = React.useState<string>('')
     const [warningActivity, setWarningActivity] = React.useState<string>('')
+    const [warningYourTarget, setWarningYourTarget] = useState<string>('')
     const [weight, setWeight] = React.useState<string>('')
     const [height, setHeight] = React.useState<string>('')
-    const [activity, setActivity] = useState(undefined)
+    const [activity, setActivity] = React.useState<string>('')
     const [yourTarget, setYourTarget] = useState<string>('')
-    const data = [
-        { label: 'Sedentary', value: '1' },
-        { label: 'Light exercise (1-2 days/week)', value: '2' },
-        { label: 'Moderate exercise (3-4 days/week)', value: '3' },
-        { label: 'Heavy exercise (6-7 days/week)', value: '4' },
-        { label: 'Athlete exercise (2x per day)', value: '5' },
+
+    const data = [ 'Sedentary',
+    'Light exercise (1-2 days/week)',
+    'Moderate exercise (3-4 days/week)',
+    'Heavy exercise (6-7 days/week)',
+    'Athlete exercise (2x per day)',
     ]
 
-    const verifyInformation = (weight: string, height: string, activity: undefined) => {
+    const verifyInformation = (weight: string, height: string, activity: string, target: string) => {
+        var  flag = 0
         setSuccess(false)
         if(weight === ''){
+            flag++
+            outputWeight = 0
             setWarningWeight('Please enter Information')
+        } else {
+            outputWeight= parseInt(weight,10)
+            setWarningWeight('')
         }
         if(height === ''){
+            flag++
+            outputHeight = 0
             setWarningHeight('Please enter Information')
+        } else {
+            outputHeight= parseInt(height,10)
+            setWarningHeight('')
         }
-        if(activity === '1'){
-            setWarningActivity('Please enter Information')
+        if(activity === ''){
+            flag++
+            outputActivity = 0
+            setWarningActivity('Please choose Information')
+        }else {
+            if(activity === 'Sedentary'){
+                outputActivity = 1
+            } else if (activity === 'Light exercise (1-2 days/week)'){
+                outputActivity = 2
+            } else if (activity ===  'Moderate exercise (3-4 days/week)') {
+                outputActivity = 3
+            } else if (activity === 'Heavy exercise (6-7 days/week)'){
+                outputActivity = 4
+            } else {
+                outputActivity = 5
+            }
+            setWarningActivity('')
         }
+        if(target === ''){
+            flag++
+            outputTarget = 0
+            setWarningYourTarget('Please choose Information')
+        } else {
+            if(target === 'Increase Weight'){
+                outputTarget = 1
+            } else if(target === 'Reduce Weight'){
+                outputTarget = 2
+            } else {
+                outputTarget = 3
+            }
+            setWarningYourTarget('')
+           
+        }
+        if(flag === 0){
+            setSuccess(true)
+        }
+        
     }
     return (
         <>
@@ -45,6 +98,7 @@ export default function Calculate({ navigation }: any) {
                 message='Your TDEE has been caculated'
                 visible={success}
                 setVisible={setSuccess}
+                handleOk = {() => navigation.goBack()}
             />
             <ScrollView contentContainerStyle={styles.container}>
                 <Text style={styles.title}>Update your status</Text>
@@ -75,7 +129,9 @@ export default function Calculate({ navigation }: any) {
                         <Dropdown
                             label='Activity'
                             data={data}
-                            onSelect={() => setActivity}
+                            onSelect={(data) => {
+                                setActivity(data)                               
+                            }}
                         />
                         <Text style={styles.warningText}>
                             {warningActivity}
@@ -102,6 +158,9 @@ export default function Calculate({ navigation }: any) {
                                     </Text>
                                 </View>
                             ))}
+                            <Text style={styles.warningText}>
+                                {warningYourTarget}
+                            </Text>
                         </View>
                     </View>
                 </View>
@@ -112,7 +171,7 @@ export default function Calculate({ navigation }: any) {
                         type='confirm'
                         arrow
                         onPress={() => {
-                            navigation.navigate('My profile')
+                            verifyInformation(weight, height, activity, yourTarget)
                         }}
                     />
                 </View>
