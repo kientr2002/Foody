93794efe9c91a8ceaf458181289de7556a7fd792
Lê Nguyễn Huyền Thoại <lenguyenhuyenthoai@gmail.com>
2023-01-15 10:55:08 +0700

    format folder

--- frontend/src/util/convertDate.tsx
+++
@@ -1,7 +0,0 @@
-const convertDate = (string: string | null): string | null => {
-    if (!string) return null
-    let date = new Date(string)
-    return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
-}
-
-export default convertDate
